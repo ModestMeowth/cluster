eafa23effb3444f2210b1765651b93278826e65b
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-12-26 19:23:44 -0600

    move to pwnyboy

--- flake.nix
+++ flake.nix
@@ -4,48 +4,45 @@
     unstable.url = "nixpkgs/nixos-unstable";
     parts.url = "flake-parts";
     devshell.url = "devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
-
-    talhlpr.url = "github:budimanjojo/talhelper";
-    talhlpr.inputs.nixpkgs.follows = "unstable";
+    devshell.inputs.nixpkgs.follows = "unstable";
   };
 
-  outputs = { ... } @ inputs:
-    inputs.parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+  outputs = { ... } @inputs: inputs.parts.lib.mkFlake { inherit inputs; } {
+    systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-      imports = [ inputs.devshell.flakeModule ];
+    imports = [
+      inputs.parts.flakeModules.easyOverlay
+      inputs.devshell.flakeModule
+    ];
 
-      perSystem = { pkgs, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.devshell.overlays.default
-            (final: _: {
-              talhelper = inputs.talhlpr.packages.${system}.default;
-            })
-            (final: _: {
-              unstable = import inputs.unstable {
-                inherit (final) system;
-                overlays = [
-                  (final: prev: {
-                    kubernetes-helm-wrapped = prev.wrapHelm prev.kubernetes-helm {
-                      plugins = with prev.kubernetes-helmPlugins; [
-                        helm-diff
-                        helm-git
-                        helm-unittest
-                      ];
-                    };
-                  })
-                ];
-              };
-            })
-          ];
-        };
+    perSystem = { pkgs, system, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.devshell.overlays.default
+          (final: _: {
+            unstable = import inputs.unstable {
+              inherit (final) system;
+              overlays = [
+                (final: prev: {
+                  kubernetes-helm-wrapped = prev.wrapHelm prev.kubernetes-helm {
+                    plugins = with prev.kubernetes-helmPlugins; [
+                      helm-diff
+                      helm-git
+                      helm-unittest
+                    ];
+                  };
+                  talhelper = pkgs.callPackage ./talhelper.nix { };
+                })
+              ];
+            };
+          })
+        ];
+      };
 
-        devShells.default = pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-        };
+      devShells.default = pkgs.devshell.mkShell {
+        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
       };
     };
+  };
 }
---
+++ talhelper.nix
@@ -0,0 +1,55 @@
+{
+  buildGo123Module,
+  fetchFromGitHub,
+  installShellFiles,
+  stdenv,
+  lib,
+}:
+
+buildGo123Module rec {
+  pname = "talhelper";
+  version = "3.0.13";
+
+  src = fetchFromGitHub {
+    owner = "budimanjojo";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-vSBhKCQXN8y3ho+mLD9aAgcQYwlst5AOXXRVpmYvt44=";
+  };
+
+  vendorHash = "sha256-qMgyXkr/b0KeCmOzxAjAQ/N5yfIVmOGmKWG26ErCSx4=";
+
+  ldflags = [ "-s -w -X github.com/budimanjojo/talhelper/v3/cmd.version=v${version}" ];
+
+  doCheck = false; # no tests
+
+  subPackages = [
+    "."
+    "./cmd"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  doInstallCheck = true;
+  installCheckPhase = ''
+    $out/bin/talhelper --version | grep ${version} > /dev/null
+  '';
+
+  postInstall = lib.optionalString (stdenv.hostPlatform == stdenv.buildPlatform) ''
+    for shell in bash fish zsh; do
+      $out/bin/talhelper completion $shell > talhelper.$shell
+      installShellCompletion talhelper.$shell
+    done
+  '';
+
+  meta = with lib; {
+    description = "A tool to help creating Talos kubernetes cluster";
+    mainProgram = "talhelper";
+    longDescription = ''
+      Talhelper is a helper tool to help creating Talos Linux cluster
+      in your GitOps repository.
+    '';
+    homepage = "https://github.com/budimanjojo/talhelper";
+    license = licenses.bsd3;
+  };
+}
